03cda0a9de2209c54f015e6a703db4072f3dce3d
KittawatN <105302372+KittawatN@users.noreply.github.com>
2022-05-14 01:10:08 +0700

    Initial commit

---
+++ src/components/von.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>V-On</h1>
+    <h2>{{ message }}</h2>
+    <botton v-on:click="add()">Add</botton>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 0,
+    };
+  },
+  methods: {
+    add() {
+      this.message = this.message + 1;
+    },
+  },
+};
+</script>
